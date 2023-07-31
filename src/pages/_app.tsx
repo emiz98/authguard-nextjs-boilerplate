55b38f50ddc1b97cd052d85f9f5279cd1b18f9a1
Menura Adithya <64089619+emiz98@users.noreply.github.com>
2023-07-31 14:46:29 +0530

    Build v1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "@/styles/globals.css";
+
+import AuthguardProvider from "../../authguard";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthguardProvider>
+      <Component {...pageProps} />
+    </AuthguardProvider>
+  );
 }
